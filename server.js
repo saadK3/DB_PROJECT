d7987bfc93aac1cfc0d70cb2d20677cad883a5ee
saad <saadahmadkhan104@gmail.com>
2024-11-23 10:42:25 +0500

    second(with node server)

---
+++ server.js
@@ -0,0 +1,19 @@
+// THIS FILE CONTAINS THE CODE FOR THE EXPRESS SERVER
+const express = require("express");
+const path = require("path");
+// Create an instance of the server in the variable app
+const app = express();
+const PORT = 8080;
+
+// Serve static files from the current directory
+app.use(express.static(__dirname));
+
+// Start from the welcome page when the server is launched
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "welcome.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log("Abdullah is gay"); // if this is printed in terminal then server has connected successfully :)
+});
